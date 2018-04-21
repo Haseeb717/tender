a66cad42c8fa9b3054e5629a77a3df4397edd8cc
haseeb <haseeb@yallamotor.com>
2018-04-21 18:44:20 +0500

    Sendgrid mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  #config.action_mailer.default_url_options = { :host => 'https://easylossrunsapp.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'https://onlinetender.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
